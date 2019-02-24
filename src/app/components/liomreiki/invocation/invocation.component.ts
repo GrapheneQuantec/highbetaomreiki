831c49cd9aae650fc3e1c868375067675afafdc4
omki <aagaardmikkeli@gmail.com>
2019-02-24 22:01:08 +0100

    invocation karaoke

--- src/app/components/liomreiki/invocation/invocation.component.ts
+++ src/app/components/liomreiki/invocation/invocation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { text } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-invocation',
@@ -16,10 +17,25 @@ export class InvocationComponent implements OnInit {
   My life’s Momentum is Beta OM Hui à
   My life’s Willpower is Beta OM Hui à
   My life’s Order is Beta OM Hui à`;
+  
+  coloredAffirmationText: string;
+  normalAffirmationText: string;
+  karaokeLetterCount: number = 0;
+  karaokeSpeed: number = 80;
+  karaokeInterval;
 
   constructor() { }
 
   ngOnInit() {
+    clearInterval(this.karaokeInterval);
+    this.karaokeInterval = setInterval(() => this.updateKaraoke(), this.karaokeSpeed);
+  }
+
+  updateKaraoke() {
+    let texts = this.invocationText;
+    this.coloredAffirmationText = texts.substring(0, this.karaokeLetterCount);
+    this.normalAffirmationText = texts.substring(this.karaokeLetterCount, texts.length);
+    this.karaokeLetterCount = (this.karaokeLetterCount + 1) % (texts.length + 1);
   }
 
 }
