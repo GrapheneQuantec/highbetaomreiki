7bd7fe6052da526ed2074827089beacd86fcddfb
Mikkel Aagaard <maa@simplesite.com>
2019-03-31 22:36:47 +0200

    lung background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  backgroundVideoId = "4XT5PsazYcM";
+  backgroundVideoId = "2v9_dUlHMxU";//4XT5PsazYcM
 
   constructor(
     public authService: AuthService,
